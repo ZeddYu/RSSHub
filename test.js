f58719bb403ece592e60703d23aae456864eb0e4
ZeddYu <zeddyu.lu@gmail.com>
2022-12-24 18:20:07 +0800

    edit usenix

--- test.js
+++ test.js
@@ -15,10 +15,10 @@ const url = 'https://www.usenix.org/';
 const list = ['spring-accepted-papers', 'summer-accepted-papers', 'fall-accepted-papers', 'winter-accepted-papers'];
 
 async function main() {
-    let items = new Map();
+    let items = [];
     for (let index = 22; index < 23; index++) {
         //check if usenix'${index} exists
-        let link = `${url}/usenixsecurity${index}/`;
+        let link = `${url}/conference/usenixsecurity${index}/`;
         try {
             let response = await got.get(link);
         } catch (error) {
@@ -28,57 +28,105 @@ async function main() {
 
         // Some usenix may not have all fourt quarter sections
         // So we need to iterate through the four sections
-        list.forEach(async (suffix) => {
-            let link = `${url}/usenixsecurity${index}/${suffix}`;
-            try {
-                let response = await got.get(link);
-                console.log;
-                const $ = cheerio.load(response.body);
-                let articles = $('article').get();
-                articles.map((article) => {
-                    const item = $(article);
-                    const title = item.find('h2.node-title > a').text().trim();
-                    const href = item.find('h2.node-title > a').attr('href');
-                    const authors = item.find('div.field.field-name-field-paper-people-text.field-type-text-long.field-label-hidden p').text();
-                    return {
-                        title: title,
-                        pubDate: new Date().toUTCString(),
-                        link: `${url}${href}`,
-                    }
-                });
-            } catch (error) {
-                console.log(error);
-                // console.error(`${link}: ${error.response.statusCode}`);
-            }
-        });
+        await Promise.all(
+            list.map(async (suffix) => {
+                let link = `${url}/conference/usenixsecurity${index}/${suffix}`;
+                try {
+                    let response = await got.get(link);
+                    const $ = cheerio.load(response.body);
+                    const time = $('meta[property=article:modified_time]').attr('content');
+
+                    let articles = $('article.node-paper').get();
+                    let promises = articles.map(async (article) => {
+                        const item = $(article);
+                        const title = item.find('h2.node-title > a').text().trim();
+                        const href = item.find('h2.node-title > a').attr('href');
+                        const authors = item.find('div.field.field-name-field-paper-people-text.field-type-text-long.field-label-hidden p').text();
+                        response = await got.get(`${url}${href}`);
+                        const $$ = cheerio.load(response.body);
+                        const description = $$('div.field-name-field-paper-description > div.field-items > div').text().replaceAll('\n', '<br>');
+
+                        return {
+                            title: title,
+                            pubDate: new Date(time).toUTCString(),
+                            link: `${url}${href}`,
+                            authors: authors,
+                            description: description,
+                        };
+                    });
+                    items = await Promise.all(promises).then((res) => {
+                        return items.concat(res);
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            })
+        );
     }
+    console.log('------------');
+    console.log(items);
 }
 
 async function test() {
+    let items = [];
     let link = `https://www.usenix.org/conference/usenixsecurity23/summer-accepted-papers`;
     try {
         let response = await got.get(link);
         const $ = cheerio.load(response.body);
         let articles = $('article.node-paper').get();
-        
+        const time = $('meta[property=article:modified_time]').attr('content');
+        console.log(time);
+
         // console.log(articles)
-        const items = articles.map((article) => {
+        let promises = articles.map(async (article) => {
             const item = $(article);
             const title = item.find('h2.node-title > a').text().trim();
             const href = item.find('h2.node-title > a').attr('href');
             const authors = item.find('div.field.field-name-field-paper-people-text.field-type-text-long.field-label-hidden p').text();
+            response = await got.get(`${url}${href}`);
+            const $$ = cheerio.load(response.body);
+            const description = $$('div.field-name-field-paper-description > div.field-items > div').text().replaceAll('\n', '<br>');
+
             return {
                 title: title,
-                pubDate: new Date().toUTCString(),
+                pubDate: new Date(time).toUTCString(),
                 link: `${url}${href}`,
-            }
+                authors: authors,
+                description: description,
+            };
         });
-        console.log(items)
-        return items;
+        items = await Promise.all(promises).then((res) => {
+            return items.concat(res);
+        });
+        // console.log(items);
+        // return items;
     } catch (error) {
         console.log(error);
         // console.error(`${link}: ${error.response.statusCode}`);
     }
+    console.log(items);
 }
 
-test();
+// test();
+main();
+
+// const array1 = [1, 4, 9, 16];
+// let arr = [];
+
+// // pass a function to map
+// arr = arr.concat(
+//     array1.map((x) => {
+//         return {
+//             a: '1',
+//         };
+//     })
+// );
+// arr = arr.concat(
+//     array1.map((x) => {
+//         return {
+//             a: '1',
+//         };
+//     })
+// );
+// console.log(arr);
+// console.log(typeof arr);
